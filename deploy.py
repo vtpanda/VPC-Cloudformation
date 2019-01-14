13ac4ec42ffa403618fd7bab4745ac2dff050828
Matt Roberts <vtpanda@users.noreply.github.com>
2019-01-14 09:29:27 -0500

    Unversioned. Continued conversion to python.

--- deploy.py
+++ deploy.py
@@ -5,8 +5,11 @@ import watchtower
 import logging
 import sys
 import json
+import datetime
 
-commandargs = '{"profile": "none", "cmd": "create", "vpcname": "WordPress", "uploadbucket": "deploymentbucket", "uploadpath": "/vpc/WordPress/"}'
+today = datetime.datetime.now()
+
+commandargs = '{"profile": "none", "cmd": "create", "vpcname": "WordPress", "uploadbucket": "deploymentbucket", "uploadpath": "vpc/WordPress/"}'
 
 if len(sys.argv) > 1:
     commandargs = sys.argv[1]
@@ -16,7 +19,7 @@ params = json.loads(commandargs)
 
 profile = params.get("profile", "none")
 
-if profile = "none":
+if profile == "none":
     session = boto3.Session()
 else:
     session = boto3.Session(profile_name=profile)
@@ -33,12 +36,14 @@ subnetstackname = vpcname + "-Subnet-" + region
 natgatewaystackname = vpcname + "-NatGateway-" + region
 naclentrystackname = vpcname + "-NaclEntry-" + region
 securitygroupstackname = vpcname + "-SecurityGroup-" + region
-deploymentfolders3 = "s3://" + uploadbucket + uploadpath
-deploymentfolderhttp = "https://s3.amazonaws.com/" + uploadbucket + uploadpath
+deploymentfolders3 = "s3://" + uploadbucket + "/" + uploadpath
+deploymentfolderhttp = "https://s3.amazonaws.com/" + uploadbucket + "/" + uploadpath
 ######
 
 
 cloudformation = session.client('cloudformation')
+createwaiter = cloudformation.get_waiter('stack_create_complete')
+deletewaiter = cloudformation.get_waiter('stack_delete_complete')
 s3 = session.client('s3')
 
 
@@ -46,7 +51,7 @@ s3 = session.client('s3')
 logging.basicConfig(level=logging.INFO)
 
 logger = logging.getLogger('VPC-load-process-' + vpcname + '-'+str((today-datetime.datetime(1970,1,1)).total_seconds()))
-logGroup = '/VPC-load-process-' + vpcname
+logGroup = '/VPC-load-process/' + vpcname
 logger.addHandler(watchtower.CloudWatchLogHandler(log_group=logGroup))
 
 
@@ -54,39 +59,81 @@ msg = 'Beginning Process'
 print(msg)
 logger.info(msg)
 
-if cmd = "create" then:
-    msg = 'Uploading Files'
-    print(msg)
-    logger.info(msg)
-    s3.upload_file(Filename="./CreateVPC.json", Bucket=uploadbucketname, Key=uploadpathname + "CreateVPC.json")
-    s3.upload_file(Filename="./CreateSubnet-US-East-1.json", Bucket=uploadbucketname, Key=uploadpathname + "CreateSubnet-US-East-1.json")
-    s3.upload_file(Filename="./CreateNatGateway-US-East-1.json", Bucket=uploadbucketname, Key=uploadpathname + "CreateNatGateway-US-East-1.json")
-    s3.upload_file(Filename="./CreateAllNetworkACLEntry.json", Bucket=uploadbucketname, Key=uploadpathname + "CreateAllNetworkACLEntry.json")
-    s3.upload_file(Filename="./CreateAllSecurityGroup.json", Bucket=uploadbucketname, Key=uploadpathname + "CreateAllSecurityGroup.json")
-    s3.upload_file(Filename="./CreateAllNetworkACLEntryNoPublicSSH.json", Bucket=uploadbucketname, Key=uploadpathname + "CreateAllNetworkACLEntryNoPublicSSH.json")
-    s3.upload_file(Filename="./CreateAllSecurityGroupNoPublicSSH.json", Bucket=uploadbucketname, Key=uploadpathname + "CreateAllSecurityGroupNoPublicSSH.json")
-    s3.upload_file(Filename="./deploy.sh", Bucket=uploadbucketname, Key=uploadpathname + "deploy.sh")
-    s3.upload_file(Filename="./deploy.py", Bucket=uploadbucketname, Key=uploadpathname + "deploy.py")
-    s3.upload_file(Filename="./deploy-parameters.json", Bucket=uploadbucketname, Key=uploadpathname + "deploy-parameters.json")
-    msg = 'Done Uploading Files'
+if cmd == "create":
+    msg = 'Create process started'
     print(msg)
     logger.info(msg)
 
 
-    msg = "Deploying the stack " + vpcstackname
+    try:
+        msg = 'Uploading Files'
+        print(msg)
+        logger.info(msg)
+        s3.upload_file(Filename="./CreateVPC.json", Bucket=uploadbucket, Key=uploadpath + "CreateVPC.json")
+        s3.upload_file(Filename="./CreateSubnet-US-East-1.json", Bucket=uploadbucket, Key=uploadpath + "CreateSubnet-US-East-1.json")
+        s3.upload_file(Filename="./CreateNatGateway-US-East-1.json", Bucket=uploadbucket, Key=uploadpath + "CreateNatGateway-US-East-1.json")
+        s3.upload_file(Filename="./CreateAllNetworkACLEntry.json", Bucket=uploadbucket, Key=uploadpath + "CreateAllNetworkACLEntry.json")
+        s3.upload_file(Filename="./CreateAllSecurityGroup.json", Bucket=uploadbucket, Key=uploadpath + "CreateAllSecurityGroup.json")
+        s3.upload_file(Filename="./CreateAllNetworkACLEntryNoPublicSSH.json", Bucket=uploadbucket, Key=uploadpath + "CreateAllNetworkACLEntryNoPublicSSH.json")
+        s3.upload_file(Filename="./CreateAllSecurityGroupNoPublicSSH.json", Bucket=uploadbucket, Key=uploadpath + "CreateAllSecurityGroupNoPublicSSH.json")
+        s3.upload_file(Filename="./deploy.sh", Bucket=uploadbucket, Key=uploadpath + "deploy.sh")
+        s3.upload_file(Filename="./deploy.py", Bucket=uploadbucket, Key=uploadpath + "deploy.py")
+        s3.upload_file(Filename="./deploy-parameters.json", Bucket=uploadbucket, Key=uploadpath + "deploy-parameters.json")
+        msg = 'Done Uploading Files'
+        print(msg)
+        logger.info(msg)
+
+
+        msg = "Deploying the stack " + vpcstackname
+        print(msg)
+        logger.info(msg)
+
+        response = cloudformation.create_stack(StackName=vpcstackname, TemplateURL=deploymentfolderhttp + "CreateVPC.json", Parameters= [ { "ParameterKey": "VPCName", "ParameterValue": vpcname } ] )
+
+        vpcstackid = response["StackId"]
+
+        createwaiter.wait(StackName=vpcstackname,
+        WaiterConfig={
+            'Delay': 30,
+            'MaxAttempts': 120
+        })
+
+        msg = vpcstackname + " Deployed Successfully with StackId: " + vpcstackid
+        print(msg)
+        logger.info(msg)
+    except:
+        msg = "Something bad just happened."
+        print(msg)
+        logger.info(msg)
+    msg = "Create process finished"
+    print(msg)
+    logger.info(msg)
+if cmd == "remove":
+    msg = 'Remove process started'
     print(msg)
     logger.info(msg)
 
-    response = cloudformation.create_stack(StackName=vpcstackname, TemplateURL=deploymentfolderhttp, Parameters= [ { "ParameterKey": "VPCName", "ParameterValue": vpcname } ]
+    try:
+        msg = "Removing the stack " + vpcstackname
+        print(msg)
+        logger.info(msg)
 
-    aws cloudformation create-stack --stack-name $vpcstackname --template-url $deploymentfolderhttp/CreateVPC.json --parameters ParameterKey=VPCName,ParameterValue=$vpcname --region $region --profile $profile
-    aws cloudformation wait stack-create-complete --stack-name $vpcstackname --region $region --profile $profile
-    if test "$?" != "0"
-    then
-      echo "$vpcstackname Did Not Return in a Timely Manner"
-      exit $?
-    fi
+        cloudformation.delete_stack(StackName=vpcstackname)
 
-    msg = vpcstackname + " Deployed Successfully"
+        deletewaiter.wait(StackName=vpcstackname,
+        WaiterConfig={
+            'Delay': 30,
+            'MaxAttempts': 120
+        })
+
+        msg = vpcstackname + " Removed Successfully"
+        print(msg)
+        logger.info(msg)
+    except:
+        msg = "Something bad just happened."
+        print(msg)
+        logger.info(msg)
+        
+    msg = 'Remove process finished'
     print(msg)
     logger.info(msg)
